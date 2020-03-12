167634275b3bd9b79ed49e5220a3cb9e40b57fff
ilyaryabikin <ilyaryabikin0708@gmail.com>
2020-03-12 23:04:19 +0300

    Added Spring backend files

--- README.md
+++
@@ -1,7 +0,0 @@
-# ITMO University's Web Programming course
-Laboratory works implemented for ITMO University's Web Programming course during the third semester.
-
-Finished works:
-- [Lab 1](https://github.com/ilyaryabikin/IFMO_Web_Programming/tree/lab-1)
-- Lab 2
-- Lab 3
---
+++ backend/src/main/java/se/ifmo/web/model/User.java
@@ -0,0 +1,53 @@
+package se.ifmo.web.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "role")
+    private String role = "user";
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
